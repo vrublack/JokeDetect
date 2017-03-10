3d41d1103c7c7095500e007f012a45d0685cf244
Valentin <valentin.rublack@gmail.com>
2017-03-10 15:47:00 -0500

    Continuous recording works

--- voice.py
+++ voice.py
@@ -1,20 +1,20 @@
 import speech_recognition as sr
-#import combined
+import combined
 
-prev_wav = []
-prev_transcription = []
+def record_phrase():
+    # Record Audio
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
 
-
-def voice_callback(recognizer, audio):
     # write audio to a WAV file
     wav_data = audio.get_wav_data()
-    prev_wav.append(wav_data)
-    if len(prev_wav) >= 3:
-        combined_wav = prev_wav[-3] + prev_wav[-2][40:] + prev_wav[-1][40:]
-        with open("mic_recording.wav", "wb") as f:
-            f.write(combined_wav)
+    wav_fname = 'mic_recording.wav'
+    with open(wav_fname, 'wb') as f:
+        f.write(wav_data)
 
-    print('Processing...')
+    print('Processing...' + '\n')
 
     # Speech recognition using Google Speech Recognition
     try:
@@ -22,21 +22,18 @@ def voice_callback(recognizer, audio):
         # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
         # instead of `r.recognize_google(audio)`
         transcribed = r.recognize_google(audio)
-        prev_transcription.append(transcribed)
-        if len(prev_transcription) >= 3:
-            combined = prev_transcription[-3] + prev_transcription[-2] + prev_transcription[-1]
-            print("Transcription: " + combined)
+        print("Transcription: " + transcribed + '\n')
+
+        word_pred = combined.word_predict(transcribed)
+        audio_pred = combined.audio_predict(wav_fname)
+        print('Word pred: {}, audio pred: {}'.format(word_pred, audio_pred))
+
+
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-
 if __name__ == '__main__':
-    # Record Audio
-    r = sr.Recognizer()
-    source = sr.Microphone()
-    print("Say something!")
-    stop_listening = r.listen_in_background(source, voice_callback)
-
-    input('Listening... Press any key to stop')
+    while True:
+        record_phrase()
